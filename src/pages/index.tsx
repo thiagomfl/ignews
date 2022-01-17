9034d72a7436975bc85e5b74ac8c74a124b669ae
Thiago <thiagomf@motorola.com>
2022-01-17 16:25:15 -0300

    First configurations

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home - IGNews</title>
+      </Head>
+      <h1>Hello IgNews</h1>
+    </>
+  );
+}
